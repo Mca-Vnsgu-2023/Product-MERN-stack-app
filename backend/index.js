0f1d0948ded773cd5f752802544894a3a1bf0f0a
dinky <mcavnsgu2023@gmail.com>
2024-01-10 11:50:31 +0530

    some code changes on index file

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
-
-require("dotenv").config();
-
 const express=require("express");
 const mongoose= require("mongoose");
 const app=express();
 const cors= require("cors")
+require("dotenv").config();
 
 app.use(express.json());
 app.use(express.Router());
